5f9a0fca0da0cccdc0fff67443761916d72f1bd6
cks2072 <cy_k7724@naver.com>
2024-06-06 22:44:38 +0900

    - new

---
+++ eclipse-workspace/FirstProject/src/main/java/chap03/CastingEx.java
@@ -0,0 +1,16 @@
+package chap03;
+
+public class CastingEx {
+
+	public static void main(String[] args) {
+		
+		// 자동형 반환 예시
+		int number = 10;
+		
+		long number2 = number ;
+		
+		System.out.println(number2);
+
+	}
+
+}
